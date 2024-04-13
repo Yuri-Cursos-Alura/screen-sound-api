9ce28cbea6172b126f88dc981c2d1e1ddbf837d4
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2024-04-13 13:58:25 -0300

    Adicionar Swagger
    Adicionado o swagger ao projeto para a documentação automática das APIs.
    
    Configurado também para abrir o swagger automáticamente ao executar a aplicação.

--- screen-sound-api/Program.cs
+++ screen-sound-api/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Json;
 using Microsoft.EntityFrameworkCore;
 using screen_sound_api.Endpoints;
 using ScreenSound.Banco;
@@ -7,12 +7,20 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.Services.Configure<JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ScreenSoundContext>();
 builder.Services.AddTransient<DAL<Artista>>();
 builder.Services.AddTransient<DAL<Musica>>();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.AddEndPointsArtistas();
 app.AddEndPointsMusicas();
